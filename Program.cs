81ace562cf761a26593d33edcce238daa616890c
Akif ul Aziz <ARr0w@users.noreply.github.com>
2024-04-16 22:19:12 +0500

    Added rest of the project files.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+﻿
 string? str = string.Empty;
 
 while (str == string.Empty)
@@ -37,4 +38,57 @@ string findpalindrome(string str)
         }
     }
     return leftMatched + rightMatched;
+}
+
+
+var data = new int[][] {
+    new int[] { 1,0 },
+    new int[] { 1,0 },
+    new int[] { 1,0 },
+    new int[] { 1,1 }
+};
+var k = 4;
+KWeakestRows(data, k);
+int[] KWeakestRows(int[][] mat, int k)
+{
+    int[] ground = new int[mat.Length];
+    int[] indexes = new int[mat.Length];
+    int[] result = new int[k];
+    for (int i = 0; i < mat.Length; i++)
+    {
+        int soldiers = 0;
+
+        for (int j = 0; j < mat[i].Length; j++)
+        {
+            if (mat[i][j] == 0)
+            {
+                break;
+            }
+            soldiers++;
+        }
+
+        ground[i] = soldiers;
+        indexes[i] = i;
+    }
+    for (int i = 0; i < mat.Length - 1; i++)
+    {
+        for (int j = i + 1; j < mat.Length; j++)
+        {
+            if (ground[indexes[i]] > ground[indexes[j]])
+            {
+                var temp = indexes[i];
+                indexes[i] = indexes[j];
+                indexes[j] = temp;
+            }
+        }
+        if (i < k)
+        {
+            result[i] = indexes[i];
+            if (mat.Length - 1 == i + 1 && i + 1 < k)
+            {
+                result[i + 1] = indexes[i + 1];
+            }
+        }
+    }
+    return result;
 }
